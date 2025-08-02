2e5d3be302b13cbed07dff17558cd020c00f33b2
Haseeb Ijaz <haseebijazi150172@gmail.com>
2025-08-02 22:02:34 +0500

    feat(vite-plugin-eslint): Add Vite Plugin for ESLint in the Vite Plugins Array.
    Further:
    1. Add Plugin Options object

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import eslintPlugin from "vite-plugin-eslint";
+
+/** CONFIGURATION OPTIONS START */
+
+// These are ESLINT Plungin Options not ESLINT Options, these are for the Plugin Configuration and not for the ESLINT Configuration. ESLING Configuration options will be declared in its own configuration file. ESLINT Options will go in eslint.config.js.
+
+const ESLINT_PLUGIN_OPTIONS = {
+  cache: false,
+  exclude: ["node_modules", "dist"],
+};
+
+/** CONFIGURATION OPTIONS END */
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), eslintPlugin(ESLINT_PLUGIN_OPTIONS)],
+});
