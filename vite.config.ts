665481aa98111fad3823df084e880ea24648c1dd
Haseeb Ijaz <haseebijazi150172@gmail.com>
2025-08-14 23:49:43 +0500

    feat(vite.config.ts): 1. Add Configurations for plugins in vite for eslint plugin config options. 2. Added configuration for plugins of SVGR and SVGO

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,23 @@ import react from "@vitejs/plugin-react";
 import eslintPlugin from "vite-plugin-eslint";
 import svgr from "vite-plugin-svgr";
 
-/** CONFIGURATION OPTIONS START */
-
-// These are ESLINT Plungin Options not ESLINT Options, these are for the Plugin Configuration and not for the ESLINT Configuration. ESLING Configuration options will be declared in its own configuration file. ESLINT Options will go in eslint.config.js.
-
-const ESLINT_PLUGIN_OPTIONS = {
-  cache: false,
-  exclude: ["node_modules", "dist", "scripts"],
-};
-
-/** CONFIGURATION OPTIONS END */
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), eslintPlugin(ESLINT_PLUGIN_OPTIONS), svgr()],
+  plugins: [
+    react(),
+    // These are ESLINT Plungin Options not ESLINT Options, these are for the Plugin Configuration and not for the ESLINT Configuration. ESLING Configuration options will be declared in its own configuration file. ESLINT Options will go in eslint.config.js.
+    eslintPlugin({
+      cache: false,
+      exclude: ["node_modules", "dist", "scripts"],
+    }),
+    svgr({
+      // https://github.com/pd4d10/vite-plugin-svgr#options
+      svgrOptions: {
+        plugins: ["@svgr/plugin-svgo", "@svgr/plugin-jsx"],
+        svgoConfig: {
+          floatPrecision: 2,
+        },
+      },
+    }),
+  ],
 });
